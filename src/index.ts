a33ce291b7c049bd50f85101d9c81bb48d8d8a1d
kelvin <kelvin950>
2023-02-06 22:18:01 +0000

    exporting modules from src

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-interface color{
-    red:number;
-    blue:number;
-    green:number ;
-}
-
-
-const color:color={
-
-    red:12 ,
-    blue:12,
-    green:12
-}
-
-
-export default color; 
+export * from './middlewares/BadInputError';
+export * from './middlewares/errorHanlder';
+export * from './utils/ErrorClass';
